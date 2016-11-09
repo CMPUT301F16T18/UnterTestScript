9dad1fdb772f28fb03113a83cf1588edae047411
Michael Lin <linzichunzf@hotmail.com>
2016-11-08 22:00:42 -0700

    update

--- test_data.sh
+++ test_data.sh
@@ -2,17 +2,24 @@ curl -XPOST http://cmput301.softwareprocess.es:8080/untert/request/9ce13093-2fde
 {
     "ID": "9ce13093-2fde-449f-9096-4a63405b6652",
     "route": {
-        "destination": {
-            "lat": 48.859019,
-            "lon": 2.293344
-        },
-        "origin": {
-            "lat": 53.621002,
-            "lon": -113.536804
-        }
+        "destination": [-113.536804, 53.621],
+        "origin": [-113.536804, 53.621002]
     },
         "isCompleted": "true",
         "requestDescription": "Someones house",
-        "riderUserName": "blablabla",
+        "riderUserName": "zhuyu",
         "estimatedFare": 100
+}'
+
+
+curl -XPOST http://cmput301.softwareprocess.es:8080/untert/request/9ce13093-2fde-449f-9096-4a63405b6333 '
+{
+    "ID": "9ce13093-2fde-449f-9096-4a63405b6652",
+    "route": {
+        "destination": [-113.536804, 53.621],
+        "origin": [-400.536804, 20.621002]
+    },
+        "requestDescription": "From University of Alberta to Roger centre",
+        "riderUserName": "zhuyu",
+        "estimatedFare": 300.222
 }'
